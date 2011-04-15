1dea0b4112ffc83792da59701981151e591158b6
lahwran <blendmaster1024@gmail.com>
2011-04-15 01:54:22 -0600

    I can walk on water

--- src/main/java/net/lahwran/bukkit/blockfilter/example/SendListener.java
+++ src/main/java/net/lahwran/bukkit/blockfilter/example/SendListener.java
@@ -17,7 +17,23 @@ import org.bukkit.event.player.PlayerListener;
 
 public class SendListener extends PlayerListener {
 	private final BlockSendExamplePlugin plugin;
-	public String[] users;
+	public usermap[] users;
+
+	public enum usermode {
+		NORMAL,
+		STONE,
+		WATERWALK;
+	}
+	
+	public class usermap {
+		public usermap(String user, usermode mode)
+		{
+			this.user = user;
+			this.mode = mode;
+		}
+		public String user;
+		public usermode mode;
+	}
 
 	public SendListener(final BlockSendExamplePlugin plugin) {
 		this.plugin = plugin;
@@ -37,26 +53,58 @@ public class SendListener extends PlayerListener {
 				
 					BufferedReader in = new BufferedReader(new FileReader(data));
 					String str;
-					ArrayList<String> builder = new ArrayList<String>();
+					ArrayList<usermap> builder = new ArrayList<usermap>();
 					while ((str = in.readLine()) != null)
 					{
 						if(str.length()>0)
 						{
-							builder.add(str.trim());
+							String[] split = str.trim().split(" ");
+							if (split.length==1)
+							{
+								builder.add(new usermap(split[0].trim(), usermode.STONE));
+							}
+							else if (split.length==2)
+							{
+								if(split[1].equalsIgnoreCase("stone"))
+								{
+									builder.add(new usermap(split[0].trim(), usermode.STONE));
+								}
+								else if (split[1].equalsIgnoreCase("waterwalk"))
+								{
+									builder.add(new usermap(split[0].trim(), usermode.WATERWALK));
+								}
+								else continue;
+							}
+							else continue;
 						}
 					}
 					in.close();
-					this.users=builder.toArray(new String[0]);
+					this.users=builder.toArray(new usermap[0]);
 			}
 			else
 			{
-				this.users = new String[]{"rock", "stone"};
+				this.users = new usermap[]{new usermap("rock", usermode.STONE), new usermap("jesus", usermode.WATERWALK)};
 				Writer out = new BufferedWriter(new FileWriter(data));
 				try 
 				{
 					for (int i=0; i<users.length; i++)
 					{
-						out.write(users[i]+"\n");
+						out.write(users[i].user+" ");
+						switch (users[i].mode)
+						{
+						case NORMAL:
+							out.write("normal");
+							break;
+						case STONE:
+							out.write("stone");
+							break;
+						case WATERWALK:
+							out.write("waterwalk");
+							break;
+						default:
+							out.write("wait, what?");
+						}
+						out.write("\n");
 					}
 				}
 				finally 
@@ -75,25 +123,42 @@ public class SendListener extends PlayerListener {
 		{
 			loadData();
 		}
-		boolean found = false;
+		usermode found = usermode.NORMAL;
 		//these string comparisons area a bad idea... probably no faster alternative
 		for(int iter=0; iter<users.length; iter++)
 		{
-			if(users[iter].equals(event.player.getName()))
+			if(users[iter].user.equals(event.player.getName()))
 			{
-				found=true;
+				found=users[iter].mode;
 				break;
 			}
 		}
-		if (found)
+		if (found != usermode.NORMAL)
 		{
-			for(int blockx=0; blockx<event.sizex; blockx++)
-			for(int blocky=0; blocky<event.sizey; blocky++)
-			for(int blockz=0; blockz<event.sizez; blockz++)
+			switch(found)
 			{
-				if(event.getBlockID(blockx, blocky, blockz) != 0)
-					event.setBlockID((byte) 1, blockx, blocky, blockz);
+			case WATERWALK:
+				for(int blockx=0; blockx<event.sizex; blockx++)
+				for(int blocky=0; blocky<event.sizey; blocky++)
+				for(int blockz=0; blockz<event.sizez; blockz++)
+				{
+					if((event.getBlockID(blockx, blocky, blockz) == 8 ||
+						event.getBlockID(blockx, blocky, blockz) == 9) &&
+						event.getBlockID(blockx, blocky+1, blockz) == 0)
+						event.setBlockID((byte) 20, blockx, blocky, blockz);
+				}
+				break;
+			case STONE:
+				for(int blockx=0; blockx<event.sizex; blockx++)
+				for(int blocky=0; blocky<event.sizey; blocky++)
+				for(int blockz=0; blockz<event.sizez; blockz++)
+				{
+					if(event.getBlockID(blockx, blocky, blockz) != 0)
+						event.setBlockID((byte) 1, blockx, blocky, blockz);
+				}
+				break;
 			}
+			
 		}
 	}
 }
